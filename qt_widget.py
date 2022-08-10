d63e2231579d8bd1bb3bc0c10dfaad36ceea8728
HEERAN <heeran3646@gmail.com>
2022-08-10 22:54:03 +0900

    Add Text browser and modify UI

--- qt_widget.py
+++ qt_widget.py
@@ -3,24 +3,27 @@ from PySide6 import QtCore, QtWidgets, QtGui
 class Custom_Widget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.init_ui()
     
-        self.title = 'py test qt widget'
-        self.button = QtWidgets.QPushButton("Button")
-        self.text = QtWidgets.QLabel("Hello World",
-                                        alignment=QtCore.Qt.AlignCenter)
-        
+    def init_ui(self):
+        self.button = QtWidgets.QPushButton("Open File")
+        self.fpath = QtWidgets.QLabel("", alignment=QtCore.Qt.AlignCenter)
+        self.msgbox = QtWidgets.QTextBrowser()
+
         self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
+        self.layout.addWidget(self.fpath)
+        self.layout.addWidget(self.msgbox)
 
         self.button.clicked.connect(self.browser_file)
 
+    def show_dialog(self):
+        self.msgbox.setPlainText("textbox")
+
+        
     def browser_file(self):
         fname = QtWidgets.QFileDialog.getOpenFileName(self,
                                         self.tr("Open file"),
-                                        self.tr("Data Files (*.csv *.xls *.xlsx);;"))
-        self.text.setText(fname[0])
+                                        self.tr("Data Files (*.xls *.xlsx);;"))
+        self.fpath.setText(fname[0])
 
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(self.title)
