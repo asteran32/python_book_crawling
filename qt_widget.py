d3495efb85aaef7cd982e17fe5f5d5701e4e357d
asterian32 <heeran3646@gmail.com>
2022-08-10 23:14:12 +0900

    Add crawling button

--- qt_widget.py
+++ qt_widget.py
@@ -1,3 +1,4 @@
+from ctypes import alignment
 from PySide6 import QtCore, QtWidgets, QtGui
 
 class Custom_Widget(QtWidgets.QWidget):
@@ -6,24 +7,29 @@ class Custom_Widget(QtWidgets.QWidget):
         self.init_ui()
     
     def init_ui(self):
-        self.button = QtWidgets.QPushButton("Open File")
+        self.fopen = QtWidgets.QPushButton("Open File")
         self.fpath = QtWidgets.QLabel("", alignment=QtCore.Qt.AlignCenter)
         self.msgbox = QtWidgets.QTextBrowser()
+        self.fstart = QtWidgets.QPushButton("Start")
 
         self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.fopen)
         self.layout.addWidget(self.fpath)
+        self.layout.addWidget(self.fstart)
         self.layout.addWidget(self.msgbox)
 
-        self.button.clicked.connect(self.browser_file)
+        self.fopen.clicked.connect(self.browser_file)
+        self.fstart.clicked.connect(self.crawling_aladin)
 
-    def show_dialog(self):
-        self.msgbox.setPlainText("textbox")
+    def show_dialog(self, text):
+        self.msgbox.setPlainText(text)
 
-        
     def browser_file(self):
         fname = QtWidgets.QFileDialog.getOpenFileName(self,
                                         self.tr("Open file"),
                                         self.tr("Data Files (*.xls *.xlsx);;"))
         self.fpath.setText(fname[0])
+
+    def crawling_aladin(self):
+        self.msgbox.setPlainText("Start crawling file {}".format(self.fpath.text()))
 
